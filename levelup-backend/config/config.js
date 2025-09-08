c95059f3a19d5936c9d133d0ba6b4b7b169e0153
Dims <mehdipares@85gmail.com>
2025-09-08 16:18:15 +0200

    test config

--- levelup-backend/config/config.js
+++ levelup-backend/config/config.js
@@ -6,6 +6,16 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 3306
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 3306,
+    logging: false
   }
 };
