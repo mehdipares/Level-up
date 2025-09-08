7c3bb841e2f11bb22712547acf5be6c3ac031cd2
Dims <mehdipares@85gmail.com>
2025-09-08 16:21:14 +0200

    tes config 2

--- levelup-backend/config/config.js
+++ levelup-backend/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -10,12 +10,11 @@ module.exports = {
     port: process.env.DB_PORT || 3306
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT || 3306,
+    use_env_variable: "DATABASE_URL",
+    dialect: process.env.DB_DIALECT || "mysql",
+    dialectOptions: {
+      ssl: { require: false }
+    },
     logging: false
   }
 };
